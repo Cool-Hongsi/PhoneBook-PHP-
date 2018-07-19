c5a23c7eccabd61d8ee44df6709a80c36319bcff
Cool-Hongsi <39789641+Cool-Hongsi@users.noreply.github.com>
2018-07-18 22:38:44 -0400

    Update read.php

--- read.php
+++ read.php
@@ -10,20 +10,20 @@
     <?php
       $conn = mysqli_connect('localhost', 'root', '111111', 'phonebook');
 
-      // 데이터 존재 여부 확인
+      // Check presense of data
       $ID = $_POST['id'];
       $result = mysqli_query($conn, "SELECT * FROM user WHERE ID = '$ID'");
       $number_of_rows = mysqli_num_rows($result);
-      if($number_of_rows == 0){ // 같은 ID 값을 가지는 데이터가 0개다.. 데이터가 없다..
+      if($number_of_rows == 0){ // the data which has same ID does not exist
         echo("There is no existing ID<br><br> Try Agian!<br>");
       }
 
-      // 데이터 존재 o
+      // presense o
       else if($number_of_rows > 0){
         $SelectSQL = "SELECT * FROM user WHERE ID = {$_POST['id']}";
         $result = mysqli_query($conn, $SelectSQL);
         $row = mysqli_fetch_array($result);
-        // 데이터베이스에 저장된 값으로 출력 $row['ID']
+        
         echo "<p>ID : ".$row['ID'].'</p>';
         echo "<p>First Name : ".$row['FIRSTNAME'].'</p>';
         echo "<p>Last Name : ".$row['LASTNAME'].'</p>';
