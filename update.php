d8f4dccadb9aaff4272a787914c0bd7a5d89c73f
Cool-Hongsi <39789641+Cool-Hongsi@users.noreply.github.com>
2018-07-18 22:39:10 -0400

    Update update.php

--- update.php
+++ update.php
@@ -16,20 +16,20 @@
       $Phone = $_POST['phone'];
       $Email = $_POST['email'];
 
-      // 데이터 존재 여부 확인
+      // Check presense of data
       $result = mysqli_query($conn, "SELECT * FROM user WHERE ID = '$ID'");
       $number_of_rows = mysqli_num_rows($result);
-      if($number_of_rows == 0){ // 같은 ID 값을 가지는 데이터가 0개다.. 데이터가 없다..
+      if($number_of_rows == 0){ // the data which has same ID does not exist
         echo("There is no existing ID<br><br> Try Agian!<br>");
       }
 
-      // 데이터 존재 o
+      // presense o
       else if($number_of_rows > 0){
         $UpdateSQL = "UPDATE user SET FIRSTNAME='$FName', LASTNAME='$LName',
         PHONE='$Phone', EMAIL='$Email' WHERE ID = '$ID'";
         $result = mysqli_query($conn, $UpdateSQL);
         $row = mysqli_fetch_array($result);
-        // 입력 받은 값을 변수에 넣고 출력 $ID = $_POST['id']
+        
         echo "<p>(Unmodifiable) ID : ".$ID.'</p>';
         echo "<p>First Name : ".$FName.'</p>';
         echo "<p>Last Name : ".$LName.'</p>';
